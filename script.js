4e3e72ede24cec25ba625ee12022ed68e48ef5b8
cloudylogs <alison.liu999@gmail.com>
2022-12-03 18:13:25 -0800

    grid
    grid complete

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
+const rainbowBtn = document.getElementById("rainbowBtn");
+const colorBtn = document.getElementById("colorBtn");
+const shadowBtn = document.getElementById("shadowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const resetBtn = document.getElementById("resetBtn");
 const slider = document.getElementById("sliderRange");
 const output = document.getElementById("rangeResult");
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-    output.innerHTML = this.value;
-}
-
 const grid = document.getElementById("grid");
 
-slider.addEventListener("change", () => {
+slider.oninput = function () {
+    output.innerHTML = `${this.value} x ${this.value}`;
+};
+
+function createGrid() {
+  slider.addEventListener("change", () => {
     let squares = slider.value;
     let gridArea = squares * squares;
+    grid.innerHTML = "";
 
     grid.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
     grid.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
 
     for (let i = 0; i < gridArea; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        grid.appendChild(square);
+      let square = document.createElement("div");
+      square.classList.add("square");
+      grid.appendChild(square);
     }
+  });
+}
 
+createGrid();
 
-})
+function colorGrid() {
+    
+}
