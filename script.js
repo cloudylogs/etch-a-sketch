e11724f0a0f1156832a5610365b1fd5ac8c9b76b
cloudylogs <alison.liu999@gmail.com>
2022-12-05 22:21:43 -0800

    reset button set
    reset button done, working on click event listener but it's not on the right element. need to figure out how to get the right thing to click

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const rainbowBtn = document.getElementById("rainbowBtn");
+// buttons
+const colorDropper = document.getElementById("colorDropper");
 const colorBtn = document.getElementById("colorBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
 const shadowBtn = document.getElementById("shadowBtn");
 const eraserBtn = document.getElementById("eraserBtn");
 const resetBtn = document.getElementById("resetBtn");
+
+// grid related
 const slider = document.getElementById("sliderRange");
 const output = document.getElementById("rangeResult");
 const grid = document.getElementById("grid");
 
+// slider input
 slider.oninput = function () {
-    output.innerHTML = `${this.value} x ${this.value}`;
+  output.innerHTML = `${this.value} x ${this.value}`;
 };
 
+// create grid
 function createGrid() {
   slider.addEventListener("change", () => {
     let squares = slider.value;
@@ -21,15 +27,22 @@ function createGrid() {
     grid.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
 
     for (let i = 0; i < gridArea; i++) {
-      let square = document.createElement("div");
+      const square = document.createElement("div");
       square.classList.add("square");
       grid.appendChild(square);
     }
   });
+  const colored = document.querySelector("square");
+  colored.addEventListener("mousedown", (e) => {
+    e.target.classList.replace("square", "color");
+  });
 }
+
+// reset button
+resetBtn.addEventListener("click", () => {
+  grid.innerHTML = "";
+  slider.value = "";
+  output.innerHTML = "slide for grid";
+});
 
 createGrid();
-
-function colorGrid() {
-    
-}
