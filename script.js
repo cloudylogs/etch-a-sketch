689e5f60b8cea69af035d8987afe27a5093e8f64
cloudylogs <alison.liu999@gmail.com>
2022-12-19 21:45:24 -0800

    buttons
    reworked buttons to do their designated tasks, but there's a bug that shows on larger grids where the pixels are not seen as grid children

--- script.js
+++ script.js
@@ -1,66 +1,95 @@
 "use strict";
-// buttons
-const colorDropper = document.getElementById("colorDropper");
-const colorBtn = document.getElementById("colorBtn");
-const rainbowBtn = document.getElementById("rainbowBtn");
-const shadowBtn = document.getElementById("shadowBtn");
-const eraserBtn = document.getElementById("eraserBtn");
-const resetBtn = document.getElementById("resetBtn");
 
-// grid related
-const slider = document.getElementById("sliderRange");
-const output = document.getElementById("rangeResult");
+// DEFAULTS
+const defaultSize = 20;
+
+// slider related
+
+
 const grid = document.getElementById("grid");
-
-// slider input
-slider.oninput = function () {
-  output.innerHTML = `${this.value} x ${this.value}`;
-};
-let squares = slider.value;
-
-function createGrid(squares) {
-  grid.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
-  grid.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
-  for (let i = 1; i < squares * squares; i++) {
+function createGrid(defaultSize) {
+  grid.style.gridTemplateRows = `repeat(${defaultSize}, 1fr)`;
+  grid.style.gridTemplateColumns = `repeat(${defaultSize}, 1fr)`;
+  for (let i = 1; i < defaultSize * defaultSize; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = "black";
+    });
     grid.appendChild(square);
   }
-  const gridPixels = document.querySelectorAll(".square");
-  gridPixels.forEach((pixel) => {
-    pixel.addEventListener("mousedown", changeColor);
-    pixel.addEventListener("mouseover", changeColor);
-    function changeColor(e) {
-      e.target.style.backgroundColor = "black";
-    }
-  });
-  
-  
-  // update grid
-  slider.addEventListener("change", () => {
-    let squares = slider.value;
-    grid.innerHTML = "";
-    grid.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
-    grid.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
-
-    for (let i = 0; i < squares * squares; i++) {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      grid.appendChild(square);
-    }
-    //change color 
-  const gridPixels = document.querySelectorAll(".square");
-  gridPixels.forEach((pixel) => {
-    pixel.addEventListener("mousedown", changeColor);
-    pixel.addEventListener("mouseover", changeColor);
-    function changeColor(e) {
-      e.target.style.backgroundColor = "black";
-    }
-  });
-  });
 }
 
+const slider = document.getElementById("sliderRange");
+const output = document.getElementById("rangeResult");
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+let squares = slider.value;
+slider.addEventListener("change", () => {
+  let squares = slider.value;
+  grid.innerHTML = "";
+  grid.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+  grid.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+  for (let i = 0; i < squares * squares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = "black";
+    });
+    grid.appendChild(square);
+  }
+});
+
+const rainbowBtn = document.getElementById("rainbowBtn");
+function getRainbow() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+rainbowBtn.addEventListener("click", function () {
+  for (let i = 0; i < squares * squares; i++) {
+    let pixel = grid.children;
+    pixel[i].addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = getRainbow();
+    });
+  }
+});
+
+const colorDropper = document.getElementById("colorDropper");
+colorDropper.addEventListener("input", function () {
+  let newColor = colorDropper.value;
+  let pixel = grid.children;
+  for (let i = 0; i < squares * squares; i++) {
+    pixel[i].addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = newColor;
+    });
+  }
+});
+
+const eraserBtn = document.getElementById("eraserBtn");
+eraserBtn.addEventListener("click", function () {
+  let pixel = grid.children;
+  for (let i = 0; i < squares * squares; i++) {
+    pixel[i].addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = "white";
+    });
+  }
+});
+
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", function(){
+  grid.innerHTML='';
+  createGrid(defaultSize);
+  slider.value = defaultSize;
+  output.innerHTML = defaultSize;
+})
 
 window.onload = () => {
-  createGrid(squares);
-}
+  createGrid(defaultSize);
+};
+
